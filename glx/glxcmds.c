83eb171dfbd7ccbac8443837311f1f949ea33afc
Robert Bragg <robert@sixbynine.org>
2014-11-20 01:30:36 +0000

    glx: use WindowGetVisual utility
    This avoids a minor duplication of code by using the WindowGetVisual
    utility function to map a visual XID to a VisualPtr in glxcmds.c
    
    Signed-off-by: Robert Bragg <robert@sixbynine.org>

--- glx/glxcmds.c
+++ glx/glxcmds.c
@@ -105,18 +105,7 @@ static int
 validGlxFBConfigForWindow(ClientPtr client, __GLXconfig * config,
                           DrawablePtr pDraw, int *err)
 {
-    ScreenPtr pScreen = pDraw->pScreen;
-    VisualPtr pVisual = NULL;
-    XID vid;
-    int i;
-
-    vid = wVisual((WindowPtr) pDraw);
-    for (i = 0; i < pScreen->numVisuals; i++) {
-        if (pScreen->visuals[i].vid == vid) {
-            pVisual = &pScreen->visuals[i];
-            break;
-        }
-    }
+    VisualPtr pVisual = WindowGetVisual((WindowPtr) pDraw);
 
     /* FIXME: What exactly should we check here... */
     if (pVisual->class != glxConvertToXVisualType(config->visualType) ||
